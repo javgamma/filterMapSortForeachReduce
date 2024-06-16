47aa0c7e4b2d49f7b67aae450a2f765dbc79649c
Javmir Gamma <javmir.hernandez@students.gammatech.schoolgit config --global user.email javmir.hernandez@students.gammatech.school>
2024-06-16 23:44:06 +0200

    database

---
+++ main.js
@@ -0,0 +1,45 @@
+// spanish_cities = [
+//     "Madrid",
+//     "Barcelona",
+//     "Valencia",
+//     "Sevilla",
+//     "Zaragoza",
+//     "Málaga",
+//     "Murcia",
+//     "Palma",
+//     "Las Palmas de Gran Canaria",
+//     "Bilbao",
+//     ];
+    
+
+
+// const baseDeDatos = [];
+
+// // Generate 3000 objects
+// for (let i = 1; i <= 3000; i++) {
+// let start_date = new Date(2024, 5, 1);
+// let end_date = new Date(2024, 5, 30);
+// let random_date = new Date(
+// start_date.getTime() +
+// Math.random() * (end_date.getTime() - start_date.getTime())
+// );
+
+// let propiedad = {
+// nombre: "Propiedad " + i,
+// direccion:
+// "Calle " +
+// Math.floor(Math.random() * 99 + 1) +
+// ", " +
+// spanish_cities[Math.floor(Math.random() * spanish_cities.length)],
+// fechaDePublicacion: random_date.toISOString().split("T")[0], // Format date as "YYYY-MM-DD"
+// cantidadDeFotos: Math.floor(Math.random() * 20 + 1),
+// metrosCuadrados: Math.floor(Math.random() * 450 + 50),
+// precioDeVenta: Math.floor(Math.random() * 950000 + 50000),
+// };
+// baseDeDatos.push(propiedad);
+// }
+
+// console.log(baseDeDatos);
+
+
+
